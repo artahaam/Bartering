22bcb9d93ebdcec354d7b5be4bc200ad280458ac
alireza tahami <alireza.thm03@gmail.com>
2025-05-08 22:33:12 +0330

    create tables users, supports, currencies, offers

---
+++ database.sql
@@ -0,0 +1,34 @@
+-- Active: 1746729928632@@127.0.0.1@5432@Bartering
+CREATE TABLE users(
+    user_id SERIAL PRIMARY KEY,
+    phone_number VARCHAR(11) NOT NULL,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    gender VARCHAR(10),
+    age INTEGER,
+    password VARCHAR(255)
+)
+
+CREATE TABLE supports(
+    support_id INTEGER REFERENCES users(user_id),
+    email VARCHAR(50) NOT NULL UNIQUE,
+    job_description TEXT
+)
+
+CREATE TABLE currencies(
+    currency_id SERIAL PRIMARY KEY,
+    is_item BOOLEAN NOT NULL,
+    is_service BOOLEAN NOT NULL,
+    name VARCHAR(30),
+    description TEXT
+)
+
+CREATE TABLE offers(
+    offer_id SERIAL PRIMARY KEY,
+    offerd_by INTEGER REFERENCES users(user_id),
+    accepted_by INTEGER REFERENCES users(user_id),
+    to_get INTEGER REFERENCES currencies(currency_id),
+    to_give INTEGER REFERENCES currencies(currency_id),
+    date DATE,
+    type VARCHAR(30)
+)
