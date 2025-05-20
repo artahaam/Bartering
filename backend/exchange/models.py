80f04418877ad6a4d529ff694a22e17df56d9d60
alireza tahami <alireza.thm03@gmail.com>
2025-05-20 23:30:27 +0330

    models for OfferProposal and set managed=False

--- backend/exchange/models.py
+++ backend/exchange/models.py
@@ -4,6 +4,13 @@ from django.utils.translation import gettext_lazy as _
 
 
 class Currency(models.Model):
+    
+    class Meta:
+        verbose_name = _('واحد تبادل')
+        db_table = 'currencies'
+        managed = False
+    
+    
     name = models.CharField(_('نام'), max_length=100)
     description = models.TextField(_('توضیحات'), blank=True)
     is_item = models.BooleanField(_('کالا'), default=True)
@@ -11,6 +18,11 @@ class Currency(models.Model):
     
 
 class Offer(models.Model):
+    class Meta:
+        verbose_name = _('آگهی')
+        db_table = 'offers'
+        managed = False
+    
     
     class Status(models.TextChoices):
         OPEN = ('open', _('فعال'))
@@ -25,3 +37,18 @@ class Offer(models.Model):
     to_give = models.ForeignKey(_('عرضه'), Currency, related_name="offers_to_give", on_delete=models.SET_NULL, null=True)
     to_get = models.ForeignKey(_('تقاضا'), Currency, related_name="offers_to_get",  on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(_('ایجاد شده در'), auto_now_add=True)
+
+
+class OfferProposal(models.Model):
+    
+    class Meta:
+        verbose_name = _('آگهی‌های داری متقاضی')
+        db_table = 'offer_proposal'
+        managed = False
+    
+    
+    offer_id = models.ForeignKey(Offer, on_delete=models.CASCADE, related_name="proposals", verbose_name=_('شماره آگهی'))
+    proposer_id = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name=_('متقاضی'))
+    proposed_currency = models.ForeignKey(Currency, null=True, blank=True, on_delete=models.SET_NULL, verbose_name=_('واحد پیشنهادی'))
+    created_at = models.DateTimeField(_('ایجاد شده در'), auto_now_add=True)
+
