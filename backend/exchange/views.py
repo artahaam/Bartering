62a682d30c14677fb72be20dcf7d0d6daa181656
alireza tahami <alireza.thm03@gmail.com>
2025-05-21 00:39:40 +0330

    currenct and offer view-sets

--- backend/exchange/views.py
+++ backend/exchange/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Offer, Currency
+from .serializers import OfferSerializer, CurrencySerializer
 
 # Create your views here.
+class CurrencyViewSet(viewsets.ModelViewSet):
+    queryset = Currency.objects.all()
+    serializer_class = CurrencySerializer
+    # permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    
+
+class OfferViewSet(viewsets.ModelViewSet):
+    queryset = Offer.objects.all()
+    serializer_class = OfferSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(offered_by=self.request.user)
+
+
