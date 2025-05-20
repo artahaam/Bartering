df06690e343347e094f8269b32d6d429fddff186
alireza tahami <alireza.thm03@gmail.com>
2025-05-21 00:42:00 +0330

    add IsOwnerOrReadOnly to permission_classes

--- backend/exchange/views.py
+++ backend/exchange/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import viewsets, permissions
 from .models import Offer, Currency
 from .serializers import OfferSerializer, CurrencySerializer
+from permissions import IsOwnerOrReadOnly
 
 # Create your views here.
 class CurrencyViewSet(viewsets.ModelViewSet):
@@ -13,7 +14,7 @@ class CurrencyViewSet(viewsets.ModelViewSet):
 class OfferViewSet(viewsets.ModelViewSet):
     queryset = Offer.objects.all()
     serializer_class = OfferSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(offered_by=self.request.user)
