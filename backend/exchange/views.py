f7d56add3b858d55f43c5d2496aa2ac111fc0a81
alireza tahami <alireza.thm03@gmail.com>
2025-05-29 00:22:13 +0330

    define filtering and search attributes

--- backend/exchange/views.py
+++ backend/exchange/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, filters
 from .models import Offer, Currency
 from .serializers import OfferSerializer, CurrencySerializer
 from .permissions import IsOwnerOrReadOnly
+from .filters import OfferFilter
 
 # Create your views here.
 class CurrencyViewSet(viewsets.ModelViewSet):
@@ -15,6 +16,11 @@ class OfferViewSet(viewsets.ModelViewSet):
     queryset = Offer.objects.all()
     serializer_class = OfferSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    filterset_class = OfferFilter
+    
+    search_fields = ['title', 'description']
+    ordering_fields = ['created_at', 'title']
+    ordering = ['-created_at']
 
     def perform_create(self, serializer):
         serializer.save(offered_by=self.request.user)
