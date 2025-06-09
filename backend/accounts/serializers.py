f71c357f0d92e194587bece30cdeee7b8f9d3323
alireza tahami <alireza.thm03@gmail.com>
2025-06-09 18:59:57 +0330

    UserCreateSerializer

---
+++ backend/accounts/serializers.py
@@ -0,0 +1,31 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+User = get_user_model()
+
+class UserCreateSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = User
+        fields = (
+            'phone_number', 
+            'password', 
+            'email', 
+            'student_id', 
+            'first_name', 
+            'last_name'
+        )
+        extra_kwargs = {'password': {'write_only': True}}
+        
+        
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            phone_number=validated_data['phone_number'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            student_id=validated_data['student_id'],
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', '')
+        )
+        return user
+        
