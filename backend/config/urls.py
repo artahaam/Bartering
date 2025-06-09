47a4b3a9bd8ec64360048a89071592b2e7de7099
alireza tahami <alireza.thm03@gmail.com>
2025-06-09 19:07:04 +0330

    setup urls for registeration and login

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('barter.urls')),
+    path('api/barter/', include('barter.urls')),
+    path('api/accounts/', include('accounts.urls', namespace='accounts')),
 ]
