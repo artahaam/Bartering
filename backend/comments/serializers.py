00d41e69a834a34f32311e39c1b9529969492f43
alireza tahami <alireza.thm03@gmail.com>
2025-06-10 23:17:09 +0330

    add ReplySerializer so we can render nested comments (replies)

--- backend/comments/serializers.py
+++ backend/comments/serializers.py
@@ -1,10 +1,20 @@
 from rest_framework import serializers
 from .models import Comment
 
-class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.StringRelatedField(read_only=True)
 
+class ReplySerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField(read_only=True)
+    
     class Meta:
         model = Comment
-        fields = ['id', 'author', 'text', 'parent', 'created_at']
+        fields = ['id', 'author', 'text', 'created_at']
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField(read_only=True)
+    replies = ReplySerializer(many=True, read_only=True)
+    class Meta:
+        model = Comment
+        fields = ['id', 'author', 'text', 'parent', 'created_at', 'replies']
         read_only_fields = ['author', 'created_at']
+        
